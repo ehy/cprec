02f1e61a1844d7be0b3c9e5a0d0b07d4513a0d19
Ed Hynan <edhynan@gmail.com>
2016-04-05 19:01:24 -0400

    Change 'code name' of front end app.

--- wxDVDBackup.py
+++ wxDVDBackup.py
@@ -312,7 +312,7 @@ _dbg(_T("debug file opened"))
 # version globals: r/o
 # jump 0.0.2.0->0.3.0 to sync with package
 version_string = _T("0.3.1")
-version_name   = _("Slither Quietly; Carry Big Stick!")
+version_name   = _("Hiss Softly; Carry Big Stick!")
 version_mjr    = 0
 version_mjrrev = 3
 version_mnr    = 1
